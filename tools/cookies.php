690f1f9935a0e9e42b0fc00e6431ee688e5fc5ab
ez666 <nick@emz.com.au>
2013-10-11 02:44:59 +1100

    naming

--- tools/cookies.php
+++ tools/cookies.php
@@ -12,7 +12,7 @@ if($_REQUEST['action'] == 'php') {
 <!DOCTYPE html>
 <html lang="en-au">
   <head>
-    <title>kos</title>
+    <title>cookies.php</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet">
   </head>
